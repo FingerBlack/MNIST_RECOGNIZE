fb232f6666c94018b0c725711b6c109c7a17ddea
FingerBlack <37107553+FingerBlack@users.noreply.github.com>
2020-12-08 21:14:04 +0800

    Optimize speed

--- recognize_digits.py
+++ recognize_digits.py
@@ -3,24 +3,14 @@ import numpy as np
 import os
 import random
 import matplotlib.pyplot as plt
-def sigmoid(z):
-    for i in range(len(z)) :
-        for j in range(len(z[i])):
-            z[i][j]=for_sigmoid(z[i][j])
-    return z
-def for_sigmoid(inx):
-    if inx>=0:      #对sigmoid函数的优化，避免了出现极大的数据溢出
-        return 1.0/(1+np.exp(-inx))
-    else:
-        return np.exp(inx)/(1+np.exp(inx))
-def d_sigmoid(z):
-    return (1-sigmoid(z))*sigmoid(z)
-
+def sigmoid(x):
+    return .5 * (1 + np.tanh(.5 * x))
+def d_sigmoid(x):
+    return (1-sigmoid(x))*(sigmoid(x))
 def fc(w,a):
     z_next=np.dot(w,a)
     a_next=sigmoid(z_next)
     return [a_next,z_next]
-
 def bc(w,z,delta_next):
     w_trans=np.transpose(w)
     delta=np.dot(w_trans,delta_next)*d_sigmoid(z)
@@ -33,7 +23,7 @@ def cost(a,y):
 def accuracy(a,y):
     a_index=[]
     y_index=[]
-    sum=0
+    sum=100
     mini_batch = len(a)
     a=np.array(a)
     y=np.array(y)
@@ -44,12 +34,12 @@ def accuracy(a,y):
     y_index=np.array(y_index)
     for i in range(0,len(a_index)):
         for j in range(0,len(a_index[i])):
-            if(a_index[i][j]==y_index[i][j]):
-                sum=sum+1
+            if(a_index[i][j]!=y_index[i][j]):
+                sum=sum-1
     #print(sum)
     return 1.0*sum/mini_batch
-train_size=60000
-test_size=10000
+train_size=10000
+test_size=1000
 train_of_img=[]
 test_of_img=[]
 train_labels=[]
@@ -57,26 +47,33 @@ test_labels=[]
 label=[0,0,0,0,0,0,0,0,0,0]
 J = []
 Acc = []
-max_epoch=10
+max_epoch=2000
 mini_batch = 100
 a=[]
 y=[]
 z=[]
-alpha=0.001
+alpha=0.0005
 delta=[]
 z.append([])
 a.append([])
 L=5
-layer_size=np.array([784, 256, 128,64,10])
+layer_size=np.array([784, 128, 64,32,10])
 w=[]
 test_list=np.load("test_list.npy")
 train_list=np.load("train_list.npy")
 test_labels=np.load("test_labels.npy")
 train_labels=np.load("train_labels.npy")
+J=list(np.load("J.npy"))
+Acc=list(np.load("Acc.npy"))
+
 w.append(np.load("w0.npy"))
 w.append(np.load("w1.npy"))
 w.append(np.load("w2.npy"))
 w.append(np.load("w3.npy"))
+
+# for iw in range(0,L-1):
+#     temp=0.05*np.random.randn(layer_size[iw],layer_size[iw+1])
+#     w.append(temp.T)
 for iter in range(0,max_epoch):
     idx = list(range(train_size))
     random.shuffle(idx)
@@ -98,7 +95,7 @@ for iter in range(0,max_epoch):
             z.append(z_temp)
             #print(len(a_temp))
         #compute delta of last layer
-        delta.append(np.array((a[L-1]-y)*a[L-1]*(1-a[L-1]) ))#10*100
+        delta.append((a[L-1]-y)*d_sigmoid(z[L-1]))#10*100
         #a[L-1]*(1-a[L-1])=f'(z[L-1])=f(z[L-1])*(1-f(z[L-1])
         #backward computation
         for l in reversed(range(1,L-1)):#3,2,1
@@ -112,15 +109,18 @@ for iter in range(0,max_epoch):
         J.append(1/mini_batch*sum(cost(a[L-1],y)))
         Acc.append(accuracy(a[L-1].T,y.T))
         print("J:%10.7f"%J[-1]+'\n'+"Acc:%10.7f"%Acc[-1]+'\n')
-        plt.plot(Acc,label='Acc')
-        plt.plot(J,label='J')
-        plt.legend()
-        plt.show()
+
         # print("----小循环完----")
+    plt.plot(Acc,label='Acc')
+    plt.plot(J,label='J')
+    plt.legend()
+    plt.show()
     np.save("w0.npy",w[0])
     np.save("w1.npy",w[1])
     np.save("w2.npy",w[2])
     np.save("w3.npy",w[3])
+    np.save("J.npy",J)
+    np.save("Acc.npy",Acc)
     print("----大循环完----")
  # step7 test the network
  # test on training set
